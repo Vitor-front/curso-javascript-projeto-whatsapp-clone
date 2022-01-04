0a3a4927f1a2a1c8904c8e7b4c295598d3f7a930
Vitor Manoel <manoelvitor440@gmail.com>
2022-01-04 11:22:12 -0300

    Update WhatsAppController.js

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -21,14 +21,17 @@ class WhatsAppController{
     elementsPrototype(){
         Element.prototype.hide = function(){
             this.style.display = 'none'
+            return this;
         }
 
         Element.prototype.show = function(){
             this.style.display = 'block'
+            return this;
         }
 
         Element.prototype.toggle = function(){
             this.style.display = (this.style.display === 'none') ? 'block' : 'none'
+            return this;
         }
 
         Element.prototype.on = function(events, fn){
@@ -36,12 +39,34 @@ class WhatsAppController{
 
                 this.addEventListener(event, fn)
             })
+            return this;
         }
 
         Element.prototype.css = function(styles){
             for (let name in styles){
                 this.style[name] = styles[name]
             }
+            return this;
         }
+
+        Element.prototype.addClass = function(name){
+            this.classList.add(name)
+            return this;
+        }
+
+        Element.prototype.removeClass = function(name){
+            this.classList.removes(name)
+            return this;
+        }
+
+        Element.prototype.toggleClass = function(name){
+            this.classList.toggle(name)
+            return this;
+        }
+
+        Element.prototype.hasClass = function(name){
+            return this.classList.contains(name)
+        }
+
     }
 }
